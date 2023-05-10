8da4197d0ae59e131235296ab84616eed1788937
edilsonfj <edilson.desenvolved@gmail.com>
2023-05-09 21:14:10 -0300

    Criação de projeto e instalação de dependências; Criação de arquivo server.ts e arquivo e diretório de rotas e variaveis de ambiente

---
+++ README.md
@@ -0,0 +1,3 @@
+# Confirm Contact
+
+Projeto do backend de uma Api de confirmação de e-mail e número de telefone, desenvolvida com Node JS e suas bibliotecas utilizando Typescript.
---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import { router } from './routes/router';
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+const app = express();
+
+app.use(express.json());
+app.use(router);
+
+app.listen(PORT, () => console.log("🖥️  Server is running!"))
